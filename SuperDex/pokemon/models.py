582d119f0e425ac9423d8cc17c8b2434ada0528f
Jeffrey Zhu <jeffreyzhu88@gmail.com>
2014-05-02 03:07:07 -0400

    Added Type model, fixed view functionality for moves and movelists
    Still need to test

--- SuperDex/pokemon/models.py
+++ SuperDex/pokemon/models.py
@@ -1,14 +1,37 @@
 from django.db import models
-from django import forms
 
 # Create your models here.
+
+class poke_type(models.Model):
+	poke_type = models.CharField(max_length=20)
+	Normal = models.DecimalField(max_digits=3, decimal_places=2)
+	Fighting = models.DecimalField(max_digits=3, decimal_places=2)
+	Flying = models.DecimalField(max_digits=3, decimal_places=2)
+	Poison = models.DecimalField(max_digits=3, decimal_places=2)
+	Ground = models.DecimalField(max_digits=3, decimal_places=2)
+	Rock = models.DecimalField(max_digits=3, decimal_places=2)
+	Bug = models.DecimalField(max_digits=3, decimal_places=2)
+	Ghost = models.DecimalField(max_digits=3, decimal_places=2)
+	Steel = models.DecimalField(max_digits=3, decimal_places=2)
+	Fire = models.DecimalField(max_digits=3, decimal_places=2)
+	Water = models.DecimalField(max_digits=3, decimal_places=2)
+	Grass = models.DecimalField(max_digits=3, decimal_places=2)
+	Electric = models.DecimalField(max_digits=3, decimal_places=2)
+	Psychic = models.DecimalField(max_digits=3, decimal_places=2)
+	Ice = models.DecimalField(max_digits=3, decimal_places=2)
+	Dragon = models.DecimalField(max_digits=3, decimal_places=2)
+	Dark = models.DecimalField(max_digits=3, decimal_places=2)
+	Fairy = models.DecimalField(max_digits=3, decimal_places=2)
+	
+	def __str__(self):
+		return self.poke_type
 
 class Pokemon(models.Model):
 	name = models.CharField(max_length=50)
 	nickname = models.CharField(max_length=50)
 	number = models.IntegerField(primary_key=True)
 	imageurl = models.URLField()
-	#poke_type = models.ForeignKey('poke_type')
+	poke_type = models.ForeignKey(poke_type)
 	genderratio = models.CharField(max_length=30)
 	catchrate = models.CharField(max_length=30)
 	egggroups = models.CharField(max_length=30)
@@ -33,8 +56,6 @@ class Pokemon(models.Model):
 	def __str__(self):
 		return self.name
 
-#class poke_type(models.Model):
-
 class Ability(models.Model):
 	ability_id = models.IntegerField(primary_key=True)
 	name = models.CharField(max_length = 20)
@@ -46,7 +67,7 @@ class has_ability(models.Model):
 	ability_id = models.ForeignKey(Ability)
 	pokemon_id = models.ForeignKey(Pokemon)
 	def __str__(self):
-		return self.ability_id
+		return self.ability_id.name
 
 class Moves(models.Model):
 	move_id = models.IntegerField(primary_key=True)
@@ -70,8 +91,9 @@ class learnset(models.Model):
 class can_learn(models.Model):
 	pokemon_id = models.ForeignKey(Pokemon)
 	move_id = models.ForeignKey(Moves)
+	TM = models.CharField(max_length=10)
 	def __str__(self):
-		return self.pokemon_id
+		return self.pokemon_id.name
 
 class Evolution(models.Model):
 	pokemon_id1 = models.ForeignKey(Pokemon, related_name='pokemon_1')
@@ -84,6 +106,3 @@ class Natures(models.Model):
 	name = models.CharField(max_length=10)
 	increased_stat = models.CharField(max_length=10)
 	descrease_stat = models.CharField(max_length=10)
-
-class SearchBox(forms.Form):
-	query = forms.CharField(max_length=100)
